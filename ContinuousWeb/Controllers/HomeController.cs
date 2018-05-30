144df888cd593b2e0cbb18ab6f3c37168ccae57b
Maharaj <niresh.maharaj@accenture.com>
2018-05-30 08:58:10 +0200

    Testing exception handling

--- ContinuousWeb/Controllers/HomeController.cs
+++ ContinuousWeb/Controllers/HomeController.cs
@@ -15,9 +15,10 @@ namespace ContinuousWeb.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+           throw new NotImplementedException();
 
-            return View();
+            //ViewData["Message"] = "Your application description page.";
+            //return View();
         }
 
         public IActionResult Contact()
